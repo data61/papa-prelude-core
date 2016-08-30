daf330579e3a0d24e6c3cb988ed4e92708950510
Tony Morris <tmorris@tmorris.net>
2016-08-30 15:15:20 +1000

    init

---
+++ src/Papa/Prelude.hs
@@ -0,0 +1,265 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Papa.Prelude(
+  module P
+) where
+
+import Prelude as P(
+--  (!!)
+    Monad
+--, encodeFloat
+--, map 
+  , scanr
+  , ($)
+  , Monoid 
+--, enumFrom
+--, mapM 
+  , scanr1
+--, ($!) 
+--, enumFromThen
+--, mapM_ 
+--, seq
+  , (&&)
+  , Num 
+--, enumFromThenTo
+  , mappend 
+--, sequence
+  , (*)
+  , Ord 
+--, enumFromTo
+--, max 
+--, sequenceA
+  , (**)
+  , Ordering(
+      LT
+    , EQ
+    , GT
+    )
+--, error 
+--, maxBound
+--, sequence_
+  , (*>) 
+  , Rational
+--, errorWithoutStackTrace
+--, maximum 
+--, show
+  , (+) 
+--, Read 
+  , even 
+  , maybe 
+--, showChar
+--, (++)
+--, ReadS 
+  , exp 
+--, mconcat 
+--, showList
+  , (-) 
+  , Real 
+  , exponent
+  , mempty 
+  , showParen
+--, (.) 
+  , RealFloat
+--, fail 
+--, min 
+--, showString
+  , (/) 
+--, RealFrac
+  , filter 
+--, minBound
+--, shows
+  , (/=) 
+  , flip 
+--, minimum 
+--, showsPrec
+  , (<) 
+--, Show 
+  , floatDigits
+  , mod 
+  , significand
+  , (<$) 
+--, ShowS 
+  , floatRadix
+  , negate 
+  , signum
+  , (<$>)
+  , String 
+  , floatRange
+  , not 
+  , sin
+  , (<*)
+  , Traversable
+  , floor 
+  , notElem 
+  , sinh
+  , (<*>)
+  , fmap 
+--, null 
+--, snd
+  , (<=)
+  , Word 
+  , foldMap 
+  , odd 
+  , span
+  , (=<<)
+  , (^)
+  , foldl 
+--, or 
+  , splitAt
+  , (==) 
+  , (^^) 
+--, foldl1 
+  , otherwise
+  , sqrt
+  , (>) 
+  , abs 
+  , foldr 
+  , pi 
+  , subtract
+  , (>=) 
+  , acos 
+--, foldr1 
+--, pred 
+--, succ
+--, (>>) 
+  , acosh 
+--, fromEnum
+--, print 
+  , sum
+  , (>>=)
+  , all 
+  , fromInteger
+  , product 
+--, tail
+  , Applicative
+  , and 
+  , fromIntegral
+  , properFraction
+  , take
+  , Bool(
+      False
+    , True
+    ) 
+  , any 
+  , fromRational
+  , pure 
+  , takeWhile
+--, Bounded 
+  , appendFile
+--, fst 
+  , putChar 
+  , tan
+  , Char 
+--, asTypeOf
+  , gcd 
+  , putStr 
+  , tanh
+  , Double 
+  , asin 
+  , getChar 
+  , putStrLn
+--, toEnum
+  , asinh 
+  , getContents
+  , quot 
+  , toInteger
+  , Either(
+      Left
+    , Right
+    ) 
+  , atan 
+  , getLine 
+  , quotRem 
+  , toRational
+  , Enum 
+  , atan2 
+--, head 
+--, read 
+  , traverse
+  , Eq 
+  , atanh 
+--, id 
+  , readFile
+  , truncate
+  , break 
+--, init 
+  , readIO 
+  , uncurry
+  , FilePath
+  , ceiling 
+  , interact
+--, readList
+--, undefined
+  , Float 
+  , compare 
+  , ioError 
+--, readLn 
+  , unlines
+  , Floating
+--, concat 
+  , isDenormalized
+--, readParen
+  , until
+  , Foldable
+--, concatMap
+  , isIEEE 
+--, reads 
+  , unwords
+  , Fractional
+--, const 
+  , isInfinite
+  , readsPrec
+  , unzip
+  , Functor 
+  , cos 
+  , isNaN 
+  , realToFrac
+  , unzip3
+  , cosh 
+  , isNegativeZero
+  , recip 
+--, userError
+  , IO 
+  , curry 
+  , iterate 
+  , rem 
+  , words
+  , IOError 
+  , cycle 
+--, last 
+  , repeat 
+  , writeFile
+  , Int 
+  , decodeFloat
+  , lcm 
+  , replicate
+  , zip
+  , Integer 
+--, div 
+--, length 
+--, return 
+  , zip3
+  , Integral
+  , divMod 
+  , lex 
+--, reverse 
+  , zipWith
+  , drop 
+  , lines 
+  , round 
+  , zipWith3
+  , dropWhile
+  , log 
+  , scaleFloat
+  , (||)
+  , either 
+  , logBase 
+  , scanl
+  , Maybe(
+      Nothing
+    , Just
+    ) 
+  , elem 
+  , lookup 
+--, scanl1
+ ) 
